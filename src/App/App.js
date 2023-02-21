650b65b4dd2a0d989483ba95d55d5450f771dbbd
Derek Yeh <derek.yeh91@gmail.com>
2023-02-21 10:22:02 -0800

    Add from setState

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Container from '../Container/Container'
 import { getReservationData } from '../apiCalls';
+import Form from '../Form/Form'
 
 class App extends Component {
 
@@ -16,7 +17,12 @@ class App extends Component {
   componentDidMount() {
     getReservationData()
     .then((data => this.setState({reservations: data})))
-    .catch(error => this.setState({error: `${error}`}))
+    .catch(error => 
+      this.setState({error: 'failed to get the data'}))
+  }
+
+  addResy(newResy) {
+    this.setState({reservations: [...this.state.reservations, newResy]})
   }
 
   render() {
@@ -24,7 +30,7 @@ class App extends Component {
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         <div className='resy-form'>
-
+          <Form addResy={this.addResy} props={this.state}/>
         </div>
         <div className='resy-container'>
           <Container reservations={this.state.reservations}/>
---
+++ src/Form/Form.js
@@ -0,0 +1,75 @@
+import React from 'react'
+import './Form.css'
+import { useState } from 'react'
+
+const Form = ({props}) => {
+
+    const [name, setName] = useState('');
+    const [date, setDate] = useState('');
+    const [time, setTime] = useState('');
+    const [guests, setGuests] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const newResy = {
+            id: Date.now(),
+            name,
+            date,
+            time,
+            guests
+        }
+        // console.log(newResy)
+        props.addResy(newResy);
+        clearInputs()
+    }
+
+    const clearInputs = () => {
+        setMessage(`Your reservation has been made!`)
+        setName('')
+        setDate('')
+        setTime('')
+        setGuests('')
+    }
+
+    return (
+        <div>
+            <form className='form'
+            onSubmit={handleSubmit}>
+                <input
+                    type='text' 
+                    id='name'
+                    placeholder='Name' 
+                    name='name' 
+                    value={name}
+                    onChange={(event) => setName(event.target.value)}
+                    required />
+                <input
+                    type='text'
+                    placeholder='Date (mm/dd)' 
+                    name='date' 
+                    value={date}
+                    onChange={(event) => setDate(event.target.value)}
+                    required />
+                <input 
+                    type='text' 
+                    placeholder='Time' 
+                    name='time' 
+                    value={time}
+                    onChange={(event) => setTime(event.target.value)}
+                    required />
+                <input 
+                    type='text' 
+                    placeholder='Number of guests' 
+                    name='guests' 
+                    value={guests}
+                    onChange={(event) => setGuests(event.target.value)}
+                    required />
+                <button type="submit" className='form-button'>Make Reservation</button>
+            </form>
+            <h2>{message}</h2>
+        </div>
+    )
+}
+
+export default Form;
