252a2aa358fe457b7f2dcd617d7c74f9193fd3b2
Derek Yeh <derek.yeh91@gmail.com>
2023-02-21 08:46:53 -0800

    Add container structure

--- src/Container/Container.js
+++ src/Container/Container.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import './Container.css'
+import Card from '../Card/Card'
 
-const Container = () => {
+const Container = ({reservations}) => {
+
+    // want to use data from api here to iterate through and return the cards for each reservation data point
+
+    const existingRes = reservations.forEach(res => {
+        return (
+            <Card 
+                id={res.id}
+                name={res.name}
+                date={res.date}
+                time={res.time}
+                number={res.number}
+            />
+        )
+    })
 
     return (
-        <Card />
+        <div>
+            {existingRes}
+        </div>
     )
 }
 
