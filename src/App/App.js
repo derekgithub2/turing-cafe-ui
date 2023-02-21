d708e834e5ffea6919a62867090e1aca587dd305
Derek Yeh <derek.yeh91@gmail.com>
2023-02-21 11:06:42 -0800

    Removed commnented refactoring

--- src/App/App.js
+++ src/App/App.js
@@ -25,46 +25,11 @@ function App() {
         <Form addResy={addResy} />
       </div>
       <div className='resy-container'>
-        { error !== '' ? error : <Container reservations={reservations}/>}
+        { error !== '' ? error && <h2>There was an issue! {error}</h2> : <Container reservations={reservations}/>}
       </div>
     </div>
   )
 }
 
-// class App extends Component {
-
-//   constructor() {
-//     super();
-//     this.state = {
-//       reservations: [],
-//       error: '',
-//     }
-//   }
-
-//   componentDidMount() {
-//     getReservationData()
-//     .then((data => this.setState({reservations: data})))
-//     .catch(error => 
-//       this.setState({error: 'failed to get the data'}))
-//   }
-
-//   addResy(newResy) {
-//     this.setState({reservations: [...this.state.reservations, newResy]})
-//   }
-
-//   render() {
-//     return (
-//       <div className="App">
-//         <h1 className='app-title'>Turing Cafe Reservations</h1>
-//         <div className='resy-form'>
-//           <Form addResy={this.addResy} props={this.state}/>
-//         </div>
-//         <div className='resy-container'>
-//           <Container reservations={this.state.reservations}/>
-//         </div>
-//       </div>
-//     )
-//   }
-// }
 
 export default App;
--- src/Container/Container.js
+++ src/Container/Container.js
@@ -1,21 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Container.css'
 import Card from '../Card/Card'
 
 const Container = ({reservations}) => {
-    
-    const existingRes = reservations.map(res => {
-        return (
-            <Card 
-                key={res.id}
-                id={res.id}
-                name={res.name}
-                date={res.date}
-                time={res.time}
-                number={res.number}
-            />
-        )
-    })
+
+    const existingRes = reservations.map(res => 
+        <Card 
+            key={res.id}
+            id={res.id}
+            name={res.name}
+            date={res.date}
+            time={res.time}
+            number={res.number}
+        />
+    )
 
     return (
         <div className="container">
