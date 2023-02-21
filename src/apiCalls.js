efda9239b2a379e6bb7b2c266914af9784194932
Derek Yeh <derek.yeh91@gmail.com>
2023-02-21 08:35:40 -0800

    Add apiCall for reservation data

---
+++ src/apiCalls.js
@@ -0,0 +1,14 @@
+const url = 'http://localhost:3001/api/v1/reservations'
+
+const getReservationData = () => {
+    return fetch(url)
+    .then(res => {
+        if(!res.ok) {
+            throw new Error('response not ok')
+            // refactor error
+        }
+        return res.json()
+    })
+}
+
+export { getReservationData }
